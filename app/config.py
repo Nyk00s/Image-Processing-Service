2992c4550a793eb93e083825d247b738e867b926
Nyk00s <nikodemfal@o2.pl>
2026-07-25 19:31:11 +0200

    alembic now don't cry because of used variables

--- app/config.py
+++ app/config.py
@@ -34,5 +34,6 @@ class Config(BaseSettings):
 
     model_config = SettingsConfigDict(
         env_file=".env",
-        env_file_encoding='utf-8'
+        env_file_encoding='utf-8',
+        extra="ignore"
     )
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
-from .picture_model import PictureModel
+from .base import Base
 from .task_model import TaskModel
 from .user_model import UserModel
 from .task_status import TaskStatus
+from .picture_model import PictureModel
 
-__all__ = ["PictureModel", "TaskModel", "UserModel", "TaskStatus"]
+__all__ = ["Base", "PictureModel", "TaskModel", "UserModel", "TaskStatus"]
